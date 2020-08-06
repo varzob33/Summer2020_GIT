b1e309bfaacff346c7dff318cd44bca708dbc328
varzob33 <varzob33@gmail.com>
2020-08-05 20:32:24 -0400

    add ReverseAstring.java

---
+++ src/Utility/ReverseAstring.java
@@ -0,0 +1,19 @@
+package Utility;
+import java.util.Scanner;
+
+public class ReverseAstring {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a String: ");//cat
+        String str = scan.nextLine();
+        //char firsr_ch = str.charAt(0);
+        //String last_ch = str.substring(str.length()- 1);
+        String reverse = "";
+        for ( int i = str.length()- 1; i >=0 ; i-- ){
+            char ch = str.charAt(i) ;
+            reverse += ch;
+        }
+        System.out.println(reverse);
+
+
+    }}
